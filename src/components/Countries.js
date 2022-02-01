34e96414908fccb1ded327fb23889cdc2333be4d
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-02 02:05:19 +0630

    react-bootstrap

---
+++ src/components/Countries.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+
+const url = "https://restcountries.com/v2/all";
+const Countries = () => {
+  return <div></div>;
+};
+
+export default Countries;
