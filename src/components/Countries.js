d28455a78024c504c0c04fffa9e1ac96394c33ae
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-03 16:45:59 +0630

    E*__ grid_need_to_be_responsive

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-
+import { Card, Row, Col } from "react-bootstrap";
 const url = "https://restcountries.com/v2/all";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
-
 
   const fetchCountryData = async () => {
     const response = await fetch(url);
@@ -17,9 +16,36 @@ const Countries = () => {
   return (
     <div>
       {countries.map((country) => {
-        const { id } = country;
+        const { flag, capital, numericCode, region, name, population } =
+          country;
 
-        return <article key={id}>Country data</article>;
+        return (
+          <Row style={{ display: "grid" }}>
+            <Col xs={1} md={2} xl={4}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={flag} />
+                <Card.Body>
+                  <Card.Title>
+                    <span>Country:</span>
+                    {name}
+                  </Card.Title>
+                  <Card.Text>
+                    <span>Capital:</span>
+                    {capital}
+                  </Card.Text>
+                  <Card.Text>
+                    <span>Region:</span>
+                    {region}
+                  </Card.Text>
+                  <Card.Text>
+                    <span>Population:</span>
+                    {population}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        );
       })}
     </div>
   );
