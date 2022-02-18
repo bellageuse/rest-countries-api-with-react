283903b16e03c2031bae9255fa858ad774b8a836
bellageuse <85023957+bellageuse@users.noreply.github.com>
2022-02-18 17:47:45 +0630

    card_done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,22 @@
 import React from "react";
-
+import "../components/Card.css";
 const Card = ({ country }) => {
   return (
     <>
-      <div className="grid">
+      <div className="card-container">
         <img src={country.flag} alt={country.name}></img>
-        <h1>{country.name}</h1>
-        <h3>
-          Population: <strong>{country.population}</strong>
-        </h3>
-        <h3>
-          Region: <strong>{country.region}</strong>
-        </h3>
-        <h3>
-          Capital: <strong>{country.capital}</strong>
-        </h3>
+        <div className="text-container">
+          <h1>{country.name}</h1>
+          <h3>
+            Population: <strong>{country.population}</strong>
+          </h3>
+          <h3>
+            Region: <strong>{country.region}</strong>
+          </h3>
+          <h3>
+            Capital: <strong>{country.capital}</strong>
+          </h3>
+        </div>
       </div>
     </>
   );
